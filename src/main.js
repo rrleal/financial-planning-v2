e41a2cb965d8b210f1dbb1b1b582d11c45d0c35c
Rogerio Leal <rgrrleal@gmail.com>
2019-09-15 23:23:57 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import '../public/css/sb-admin-2.css'
 
 Vue.config.productionTip = false
 
