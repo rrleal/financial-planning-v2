c7db4d18d1f348479d6ff154b09037ecd2c2c718
Rogério R. Leal <rogerio.leal@santacasa.org.br>
2019-09-17 17:51:27 -0300

    Layout initiated

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">{{ title }}</h1>
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+        name: "ContentPageHeader",
+
+        props: ['title']
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <Sidebar/>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <Navbar/>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <ContentPageHeader title="Painel de controle"></ContentPageHeader>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+</template>
+
+<script>
+
+    import Sidebar from './Sidebar';
+    import Navbar from './Navbar';
+    import ContentPageHeader from './ContentPageHeader';
+
+    export default {
+
+        name: 'Layout',
+
+        components: {
+            Sidebar,
+            Navbar,
+            ContentPageHeader
+        },
+
+        props: {}
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
